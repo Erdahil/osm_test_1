96d863bccdd6501e80713f28f1060be1c29ed5f7
Paweł Charysz <pawel.erdahil@gmail.com>
2025-01-02 02:45:20 +0100

    dodane waypointy i mierzenie w czasie rzeczywistym odległości od nich

--- lib/main.dart
+++ lib/main.dart
@@ -37,14 +37,28 @@ class _OSMFlutterMapState extends State<OSMFlutterMap> {
   late MapController mapController;
   LatLng? currentPosition;
   double currentZoom = 15.0;
-
+  List<Marker> markers = [];
+  double distanceToWaypoint = 0;
   @override
   void initState()
   {
     super.initState();
     mapController = MapController();
 
-    
+    _startTracking();
+  }
+
+  void _startTracking() {
+    final stream = const LocationMarkerDataStreamFactory().fromGeolocatorPositionStream();
+
+    stream.listen((LocationMarkerPosition? position) {
+      if (position != null) {
+        setState(() {
+          currentPosition = position.latLng;
+          
+        });
+      }
+    });
   }
  
   void moveToCurrentPosition() async {
@@ -59,12 +73,55 @@ class _OSMFlutterMapState extends State<OSMFlutterMap> {
 
   }
 
+  void addWaypoint(LatLng point)
+  {
+    setState(() {
+      markers.clear();
+      markers.add(
+        Marker(
+          point: point,
+          width: 37,
+          height: 37,
+          rotate: true,
+          //anchorPos: AnchorPos.align(AnchorAlign.center),
+          child: const Icon(
+            Icons.location_pin,
+            color: Colors.red,
+            size:40,
+          ),
+          alignment: Alignment.topCenter,
+          //anchorPos: const Offset(0.5, 0.5)
+          
+        ),
+      );
+    });
+  }
+
+  double calculateDistance(LatLng point1, LatLng point2)
+  {
+    final Distance distance = const Distance();
+    return distance.as(LengthUnit.Meter, point1, point2);
+  }
+/*
+  void updatePosition(LatLng position)
+  {
+    setState(() {
+      currentPosition = position;
+    });
+  }*/
+
+
   //bool isLocationPressed = false;
   bool isTracking = false;
   bool headingReset = false;
 
   @override
   Widget build(BuildContext context) {
+
+    final double distanceToWaypoint = (currentPosition != null && markers.isNotEmpty)
+      ? calculateDistance(currentPosition!, markers.last.point)
+      : 0.0;
+
     return Scaffold(
       body:Stack(
         children: [
@@ -74,6 +131,9 @@ class _OSMFlutterMapState extends State<OSMFlutterMap> {
                 initialZoom: 5,
                 minZoom: 0,
                 maxZoom: 19,
+                onLongPress: (tapPosition, point) {
+                  addWaypoint(point);
+                },
                 onPositionChanged: ( position, hasGesture) {
                   setState(() {
                     currentZoom = position.zoom;
@@ -95,7 +155,15 @@ class _OSMFlutterMapState extends State<OSMFlutterMap> {
                     marker: DefaultLocationMarker(),
                     markerDirection: MarkerDirection.heading,
                   ),
+                  /*positionStream: (PositionStream?) {
+                    PositionStream?.listen((position) {
+                      final LatLng newPosition = LatLng(position.latitude, position.longitude);
+                      updatePosition(newPosition);
+                      //mapController.move(newPosition, currentZoom); // Możesz również przesunąć mapę, aby pokazać nową lokalizację
+                    });
+                  },*/
                 ),
+              MarkerLayer(markers: markers),
               /*CurrentLocationLayer(
                 alignPositionOnUpdate: isTracking ? AlignOnUpdate.always : AlignOnUpdate.never,
                 alignDirectionOnUpdate: AlignOnUpdate.never,
@@ -163,6 +231,7 @@ class _OSMFlutterMapState extends State<OSMFlutterMap> {
               
               backgroundColor: isTracking ? Colors.blue : Colors.white,
               onPressed: () {
+                markers.clear();
                 mapController.rotate(0.0);
               },
               child: Transform.rotate(
@@ -171,7 +240,33 @@ class _OSMFlutterMapState extends State<OSMFlutterMap> {
               ),
             )
             
-          )
+          ),
+          AnimatedPositioned(
+            duration: const Duration(milliseconds: 300),
+            curve: Curves.easeInOut,
+            bottom: isTracking && markers.isNotEmpty == true ? 20 : -100,
+            left: 20,
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                decoration: BoxDecoration(
+                  color: Colors.blueAccent,
+                  borderRadius: BorderRadius.circular(8),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.2),
+                      blurRadius: 4,
+                      offset: const Offset(0, 2),
+                    )
+                  ]
+                ),
+                child: Text(
+                  '${distanceToWaypoint.toStringAsFixed(0)} m',
+                  //'${currentPosition!.latitude.toStringAsFixed(4)}, ${currentPosition!.longitude.toStringAsFixed(4)}',
+                  style: const TextStyle(color: Colors.white, fontSize: 16),
+                  
+                ),
+            )
+          ),
 
         ]
       ),
